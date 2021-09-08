ee5233c1b0219d757f0cfa9830ef4891ae18168c
sebasrengi <sebas.rengi@hotmail.com>
2021-09-07 20:44:54 -0700

    commit

---
+++ 0x01-python-if_else_loops_functions/2-print_alphabet.py
@@ -0,0 +1,5 @@
+#!/usr/bin/python3
+for a in range(ord('a'), ord('z') + 1):
+    print("{:c}".format(a), end='')r/bin/python3
+    for a in range(ord('a'), ord('z') + 1):
+            print("{:c}".format(a), end='')~
