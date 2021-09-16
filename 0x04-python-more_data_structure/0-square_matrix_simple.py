d3d7a4e7b3eef0e01974381d0963865e7c27ad76
sebasrengi <sebas.rengi@hotmail.com>
2021-09-15 23:34:35 -0500

    prueba2

--- 0x04-python-more_data_structure/0-square_matrix_simple.py
+++ 0x04-python-more_data_structure/0-square_matrix_simple.py
@@ -1,3 +1,4 @@
 #!/usr/bin/python3
 def square_matrix_simple(matrix=[]):
-    return [list(map((lambda x: x * x), elm)) for elm in matrix]
+    if matrix:
+        return [[i**2 for i in row] for row in (matrix)]
--- 0x04-python-more_data_structure/2-uniq_add.py
+++ 0x04-python-more_data_structure/2-uniq_add.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 def uniq_add(my_list=[]):
-    add = 0
-    for i in set(my_list):
-        add += i
-    return add
+        new_list = []
+        for i in my_list:
+            if i not in new_list:
+                new_list.append(i)
+        return sum(new_list)
