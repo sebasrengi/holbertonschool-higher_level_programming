30a00b0cdd0d7ccb80299455300cca4d228a9d4e
sebasrengi <sebas.rengi@hotmail.com>
2021-10-06 19:47:16 -0500

    point2_fixed

--- 0x07-python-test_driven_development/2-matrix_divided.py
+++ 0x07-python-test_driven_development/2-matrix_divided.py
@@ -1,7 +1,10 @@
 #!/usr/bin/python3
 """
+
 This is the "2-matrix_divided" module.
+
 The 2-matrix_divided module supplies one function, matrix_divided(matrix, div).
+
 """
 
 def matrix_divided(matrix, div):
@@ -9,5 +12,21 @@ def matrix_divided(matrix, div):
     if type(matrix) is not list:
         raise TypeError(
             "matrix must be a matrix (list of lists) of integers/floats")
-    
+    length = None
     for i in matrix:
+        if type(i) is not list:
+            raise TypeError(
+                "matrix must be a matrix (list of lists) of integers/floats")
+        if length is None:
+            length = len(i)
+        elif length != len(i):
+            raise TypeError("Each row of the matrix must have the same size")
+        for n in i:
+            if type(n) is not int and type(n) is not float:
+                raise TypeError("matrix must be a matrix (list of lists) of \
+                integers/floats")
+    if type(div) is not int and type(div) is not float:
+        raise TypeError("div must be a number")
+    if div == 0:
+        raise ZeroDivisionError("division by zero")
+    return [[round(n / div, 2) for n in i] for i in matrix]
