7a765cd577e711aa92481231857e841bf192e3e0
sebasrengi <sebas.rengi@hotmail.com>
2021-10-09 22:22:49 -0500

    commit

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -9,12 +9,12 @@ def add_integer(a, b=98):
     """
     function return addition
     """
-    if a != a:
-        a = 89
-    if b != b:
-        b = 89
     if type(a) not in [int, float]:
         raise TypeError("a must be an integer")
     if type(b) not in [int, float]:
         raise TypeError("b must be an integer")
+    if type(a) is float:
+        a = int(a)
+    if type(b) is float:
+        b = int(b)
     return int(a) + int(b)
