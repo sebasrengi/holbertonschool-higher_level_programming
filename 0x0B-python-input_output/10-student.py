6b4759d2d18fa150e44a3b5754b1d6d206383a99
sebasrengi <sebas.rengi@hotmail.com>
2021-10-13 10:29:29 -0500

    point10

---
+++ 0x0B-python-input_output/10-student.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+"""
+Contains the class "Student"
+"""
+
+
+class Student:
+    """Representation of a student"""
+    def __init__(self, first_name, last_name, age):
+        """Initializes the student"""
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """returns a dictionary representation of a Student instance
+        with specified attributes"""
+        if attrs is None:
+            return self.__dict__
+
+        Newdict = {}
+        for i in attrs:
+            try:
+                Newdict[i] = self.__dict__[i]
+            except:
+                pass
+        return Newdict
