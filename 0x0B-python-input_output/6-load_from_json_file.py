e99f0a0987e86d65b051f3ac71a37dc72fd55a6d
sebasrengi <sebas.rengi@hotmail.com>
2021-10-12 22:57:58 -0500

    fixed2

--- 0x0B-python-input_output/6-load_from_json_file.py
+++ 0x0B-python-input_output/6-load_from_json_file.py
@@ -5,7 +5,6 @@ Contains the "from_json_string" function
 
 import json
 
-
 def load_from_json_file(filename):
     """creates an Object from a "JSON file" """
     with open(filename, 'r', encoding='utf-8') as f:
--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -8,11 +8,11 @@ save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
 load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 
 
-List = []
+NewList = []
 try:
-    List = load_from_json_file("add_item.json")
+    NewList = load_from_json_file("add_item.json")
 except FileNotFoundError:
-    List = []
+    NewList = []
 for args in argv[1:]:
     NewList.append(args)
 save_to_json_file(NewList, "add_item.json")
