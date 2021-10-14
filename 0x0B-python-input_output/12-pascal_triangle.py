0c52cf3574ca754016fe10175ab6aba6d628dbb0
sebasrengi <sebas.rengi@hotmail.com>
2021-10-13 23:23:07 -0500

    last_commit

---
+++ 0x0B-python-input_output/12-pascal_triangle.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+"""
+Contains the pascal_triangle
+"""
+
+
+def pascal_triangle(n):
+    """returns a list of lists of integers
+    representing the Pascal’s triangle"""
+    if n <= 0:
+        return []
+
+    limit = n - 1
+    triangle = [[1]]
+
+    for i in range(limit):
+        row = []
+        row.append(1)
+
+        if len(triangle[i]) > 1:
+            prev_row_len = len(triangle[i]) - 1
+            nxt = 1
+
+            for j in range(prev_row_len):
+                suma = triangle[i][j] + triangle[i][nxt]
+                row.append(suma)
+                nxt += 1
+
+        row.append(1)
+        triangle.append(row)
+    return triangle
