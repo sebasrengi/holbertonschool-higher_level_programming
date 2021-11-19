0e1a565910d1617ab40b17739fb3a22d1a2d3c5f
sebasrengi <sebas.rengi@hotmail.com>
2021-11-18 23:56:59 -0500

    fixed

--- 0x0E-SQL_more_queries/16-shows_by_genre.sql
+++ 0x0E-SQL_more_queries/16-shows_by_genre.sql
@@ -1,9 +1,4 @@
--- lists all Comedy shows in the database hbtn_0d_tvshows.
--- The tv_genres table contains only one record where name = Comedy (but the id can be different)
--- Each record should display: tv_shows.title
--- Results must be sorted in ascending order by the show title
--- You can use only one SELECT statement
--- The database name will be passed as an argument of the mysql command
+-- My sql scrip
 
 SELECT tv_shows.title
 FROM tv_shows
