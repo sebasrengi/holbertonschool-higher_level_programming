96d96b00097fcf8dd0c5212fd1ec5d7924ce8865
sebasrengi <85517658+sebasrengi@users.noreply.github.com>
2022-04-04 15:31:47 -0500

    Update 2-arguments.js

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,12 +1,12 @@
 #!/usr/bin/node
 const ARGS = process.argv.length;
 
-    if (ARGS === 2){
-        console.log("No argument");
-        }
-    else if(ARGS === 3){    
-        console.log("Argument found");
-            }
-    else {
-        console.log('Arguments found');
-    }
+if (ARGS === 2){
+console.log("No argument");
+}
+else if(ARGS === 3){    
+console.log("Argument found");
+}
+else {
+console.log('Arguments found');
+}
