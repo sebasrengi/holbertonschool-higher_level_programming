f06552d4ea8d9f285a84dd9f1f6010a9ee2d8196
sebasrengi <85517658+sebasrengi@users.noreply.github.com>
2022-04-04 16:16:14 -0500

    Update 3-value_argument.js

--- 0x12-javascript-warm_up/3-value_argument.js
+++ 0x12-javascript-warm_up/3-value_argument.js
@@ -1,8 +1,8 @@
 #!/usr/bin/node
-const ARG_LENGTH = process.argv[2];
+const path= process.argv[2];
 
-if (ARG_LENGTH === undefined) {
+if (path === undefined) {
     console.log('No argument');
 } else {
-    console.log(ARG_LENGTH);
+    console.log(path);
 }
