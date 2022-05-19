425acd2aa7c25279cab5db9536d4f055375ff21e
sebasrengi <85517658+sebasrengi@users.noreply.github.com>
2022-05-18 21:33:21 -0500

    Update 3-methods.sh

--- 0x10-python-network_0/3-methods.sh
+++ 0x10-python-network_0/3-methods.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # Write a Bash script that takes in a URL and displays all HTTP methods the server will accept.
-curl -sI "$1" | grep Allow | cut -b 8-
+curl -sI $1 | grep Allow | cut -b 8-
