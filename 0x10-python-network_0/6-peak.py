ede9215ce8f3390df5fd3301b4f6a547414bab62
sebasrengi <85517658+sebasrengi@users.noreply.github.com>
2022-05-18 22:04:14 -0500

    fixed

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -12,16 +12,3 @@ def find_peak(list_of_integers):
         if max_i is None or max_i < ele:
             max_i = ele
     return max_i
-
-
-def function(list, start, end):
-    """"function"""
-    mid = int((start + end) / 2)
-
-    if mid + 1 >= len(list) or list[mid + 1] <= list[mid]:
-        if mid - 1 < 0 or list[mid - 1] <= list[mid]:
-            return list[mid]
-        else:
-            return function(list, 0, mid)
-    else:
-        return function(list, mid + 1, end)
