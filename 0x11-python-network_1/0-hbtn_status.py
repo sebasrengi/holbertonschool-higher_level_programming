0d07b43b4cf6dbf0c66210942493863f03426af6
sebasrengi <85517658+sebasrengi@users.noreply.github.com>
2022-05-19 15:31:11 -0500

    Update 0-hbtn_status.py

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,14 +1,30 @@
 #!/usr/bin/python3
-"""
-fetches https://intranet.hbtn.io/status
-"""
-from urllib import request
+"""For a given employee ID, returns information about
+their TODO list progress"""
 
-if __name__ == '__main__':
-    req = request.Request('https://intranet.hbtn.io/status')
-    with request.urlopen(req) as status:
-        html = status.read()
-        print("Body response:$")
-        print("\t- type: {}$".format(type(html)))
-        print("\t- content: {}$".format(html))
-        print("\t- utf8 content: {}$".format(html.decode('utf-8')))
+import requests
+import sys
+
+if __name__ == "__main__":
+
+    userId = sys.argv[1]
+    user = requests.get("https://jsonplaceholder.typicode.com/users/{}"
+                        .format(userId))
+
+    name = user.json().get('name')
+
+    todos = requests.get('https://jsonplaceholder.typicode.com/todos')
+    totalTasks = 0
+    completed = 0
+
+    for task in todos.json():
+        if task.get('userId') == int(userId):
+            totalTasks += 1
+            if task.get('completed'):
+                completed += 1
+
+    print('Employee {} is done with tasks({}/{}):'
+          .format(name, completed, totalTasks))
+
+    print('\n'.join(["\t " + task.get('title') for task in todos.json()
+          if task.get('userId') == int(userId) and task.get('completed')]))
